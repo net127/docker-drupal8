fa35cc4b0c7e0ca2c3a23f7c1b0db17d51bfe892
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-01-03 23:19:31 -0500

    Create pre-conf.sh

---
+++ pre-conf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+/usr/bin/mysqld_safe &
+ sleep 10s
+
+ mysqladmin -u root password mysqlpsswd
+ mysqladmin -u root -pmysqlpsswd reload
+ mysqladmin -u root -pmysqlpsswd create drupal
+
+ echo "GRANT ALL ON drupal.* TO drupaluser@localhost IDENTIFIED BY 'drupaldbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
+
+ wget http://ftp.drupal.org/files/projects/drupal-8.x-dev.tar.gz
+ tar -zxvf drupal-8.x-dev.tar.gz
+ rm drupal-8.x-dev.tar.gz
+ mv drupal-8.x-dev/* drupal-8.x-dev/.htaccess /var/www
+ rm -r drupal-8.x-dev
+ mkdir /var/www/sites/default/files
+ chmod a+w /var/www/sites/default/files
+ rm /var/www/index.html
+ cp /var/www/sites/default/default.settings.php /var/www/sites/default/settings.php
+ chmod a+w /var/www/sites/default/settings.php
+ 
+ a2enmod rewrite
+
+
+killall mysqld
+sleep 10s
+
