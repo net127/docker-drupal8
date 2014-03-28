ad0219b45a2aafac1143de5ce3ebaf08c326f3ce
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-03-27 21:59:21 -0400

    Update pre-conf.sh
    change to drupal 8.0 alpha10

--- pre-conf.sh
+++ pre-conf.sh
@@ -9,8 +9,8 @@
 
  echo "GRANT ALL ON drupal.* TO drupaluser@localhost IDENTIFIED BY 'drupaldbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
 
- wget http://ftp.drupal.org/files/projects/drupal-8.0-alpha8.tar.gz
- tar -zxvf drupal-8.0-alpha8.tar.gz
+ wget http://ftp.drupal.org/files/projects/drupal-8.0-alpha10.tar.gz
+ tar -zxvf drupal-8.0-alpha10.tar.gz
  rm drupal-8.0-alpha8.tar.gz
  mv drupal-8.0-alpha8/* drupal-8.0-alpha8/.htaccess /var/www
  rm -r drupal-8.0-alpha8
