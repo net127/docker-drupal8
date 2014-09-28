ebd97fb63f9a0ed2bd09f7ca0615975b7a0410a5
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-28 18:14:50 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -9,19 +9,17 @@
 
  echo "GRANT ALL ON drupal.* TO drupaluser@localhost IDENTIFIED BY 'drupaldbpasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
 
- wget http://ftp.drupal.org/files/projects/drupal-8.0.x-dev.tar.gz
- tar -zxvf drupal-8.0.x-dev.tar.gz
- rm drupal-8.0.x-dev.tar.gz
- mv drupal-8.0.x-dev/* drupal-8.0.x-dev/.htaccess /var/www
+ wget http://ftp.drupal.org/files/projects/drupal-8.0.0-alpha15.tar.gz
+ tar -zxvf drupal-8.0.0-alpha15.tar.gz
+ rm drupal-8.0.0-alpha15.tar.gz
+ mv drupal-8.0.0-alpha15/* drupal-8.0.0-alpha15/.htaccess /var/www
  mkdir /var/www/sites/default/files
  chmod a+w /var/www/sites/default/files
  rm -R /var/www/html
  cp /var/www/sites/default/default.settings.php /var/www/sites/default/settings.php
  chmod a+w /var/www/sites/default/settings.php
- 
  a2enmod rewrite
-
-
+ 
 killall mysqld
 sleep 10s
 
