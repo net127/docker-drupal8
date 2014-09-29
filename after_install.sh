09e2b36e3c52603615baed863bf2a1e846bf406d
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-29 15:38:19 -0400

    Create after_install.sh

---
+++ after_install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+### In after_install.sh (make sure this file is chmod +x):
+chmod go-w /var/www/sites/default/settings.php
+chmod go-w /var/www/sites/default/services.yml
+chmod go-w /var/www/sites/default
